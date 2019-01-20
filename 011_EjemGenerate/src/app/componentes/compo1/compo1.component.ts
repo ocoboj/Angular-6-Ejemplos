5e9f12cd4ed6c0588054146e61452e2fc950013b
Olga Conesa <olgaconesaboj@gmail.com>
2019-01-20 12:12:17 +0100

    Generar nuevo proyecto
    Saber como se crea un proyecto con generate

---
+++ 011_EjemGenerate/src/app/componentes/compo1/compo1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compo1',
+  templateUrl: './compo1.component.html',
+  styleUrls: ['./compo1.component.css']
+})
+export class Compo1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
