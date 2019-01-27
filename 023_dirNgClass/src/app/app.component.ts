5ab74511a1d9f0fd26f37f1b5856db332f1cfbe5
Olga Conesa <olgaconesaboj@gmail.com>
2019-01-27 10:49:19 +0100

    Directivas
    Directiva ngClass

---
+++ 023_dirNgClass/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  title = '023_dirNgClass';
+
+  fondoAmarillo=false;
+  letraGrande=false;
+
+  checkFondo(){
+    this.fondoAmarillo=!this.fondoAmarillo;
+  }
+
+  checkLetra(){
+    this.letraGrande=!this.letraGrande;
+  }
+
+  asignaClases(){
+    let classes = {
+      fa: this.fondoAmarillo,
+      lg: this.letraGrande
+    };
+    return classes;
+  }
+}
