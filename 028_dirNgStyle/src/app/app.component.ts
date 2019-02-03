9366666d04b652c996f04878b5af81760c29e113
Olga Conesa <olgaconesaboj@gmail.com>
2019-02-03 18:32:25 +0100

    Directiva: NgStyle
    Directiva NgStyle

---
+++ 028_dirNgStyle/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '028_dirNgStyle';
+  color = 'blue';
+  hayLetraGrande:boolean = false;
+  hayColorFondo:boolean = false;
+  hayLetraColor:boolean = false;
+  hayLetraItalica:boolean = false;
+
+  defineEstilos() {
+    let styles = {
+      
+      'font-size': this.hayLetraGrande ? '300%' : 'inital',
+      'background-color': this.hayColorFondo ? '#58FA58' : '#FFFFFF',
+      'color': this.hayLetraColor ? 'red' : 'black',
+      'font-style': this.hayLetraItalica ? 'italic' : 'normal'
+    };
+    return styles;
+  }
+}
