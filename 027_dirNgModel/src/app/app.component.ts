561fb6884ad8c92c15919aa19d3b03efb893c339
Olga Conesa <olgaconesaboj@gmail.com>
2019-02-03 12:52:52 +0100

    Directiva: ngModel
    Directiva NgModel

---
+++ 027_dirNgModel/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '027_dirNgModel';
+  nombre = '';
+  nombre2 = '';
+
+  cambiaNombre(){
+    this.nombre = this.nombre.toLowerCase();
+    this.nombre2 = this.nombre.toUpperCase();
+  }
+
+  cambiaNombre2(){
+    this.nombre2 = this.nombre2.toUpperCase();
+    this.nombre = this.nombre2.toLowerCase();
+  }
+
+}
