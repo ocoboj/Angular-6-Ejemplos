4b324d721a133e688619ecc9ea64a81d8385f85b
Olga Conesa <olgaconesaboj@gmail.com>
2019-02-10 12:19:26 +0100

    dirNgMezcla
    Ejemplo usando diferentes directivas

---
+++ 029_dirNgMezcla/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '029_dirNgMezcla';
+  mostrar = false;
+  numeros: number [] = [1,2,3,4,5,6];
+  pares: boolean = true; 
+  impares: boolean = true;
+  colorPar = 'blue'; 
+  colorImpar = 'red';
+
+  clActividad(valor){
+    let classes = { activo: valor, inactivo: !valor};
+    return classes;
+  }
+}
