91bbe330cecdba0f1391850c08566f362937b255
Olga Conesa <olgaconesaboj@gmail.com>
2019-02-17 13:09:22 +0100

    Pipes
    Pipes: DecimalPipe, CurrencyPipe y PercentPipe

---
+++ 032_pipes/src/app/number-pipes/number-pipes.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NumberPipesComponent } from './number-pipes.component';
+
+describe('NumberPipesComponent', () => {
+  let component: NumberPipesComponent;
+  let fixture: ComponentFixture<NumberPipesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NumberPipesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NumberPipesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
